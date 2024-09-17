1f91d6467b733b8fc93d8bf648b55fdb030c091a
shivareddy1287 <shivareddy1287@gmail.com>
2024-09-17 21:40:45 +0530

    updates cloudinary

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,15 +1,15 @@
 const cloudinary = require("cloudinary");
 
-cloudinary.config({
-  cloud_name: "dzrc9ejln",
-  api_key: "363267481229718",
-  api_secret: "Kihl35YQ4NQQoaClDbcu7VaC_yI",
-});
 // cloudinary.config({
-//   cloud_name: "dlxxvqogg",
-//   api_key: "839284896822179",
-//   api_secret: "58jvHlaN2bWq3672Vygw9fFWlYc",
+//   cloud_name: "dzrc9ejln",
+//   api_key: "363267481229718",
+//   api_secret: "Kihl35YQ4NQQoaClDbcu7VaC_yI",
 // });
+cloudinary.config({
+  cloud_name: "dlxxvqogg",
+  api_key: "839284896822179",
+  api_secret: "58jvHlaN2bWq3672Vygw9fFWlYc",
+});
 
 const cloudinaryUploadImg = async (fileToUpload) => {
   try {
